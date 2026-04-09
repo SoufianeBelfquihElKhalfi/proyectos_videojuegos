c85155defa38d2a7c82c2c85e13b72c3570d9821
Soufiane <soufianebelfquih828@gmail.com>
2026-04-09 15:18:32 +0200

    escena de prueba de vida y fondo UI
    Se ha actualizado la escena de prueba con un entorno completo que incluye escenario, enemigos, NavMesh y el sistema de UI. También se ha añadido el asset de fondo para la interfaz de corazones y se ha clarificado el tooltip en el script de daño.

--- Assets/__Test/Sistema de vida/InfligirDanio.cs
+++ Assets/__Test/Sistema de vida/InfligirDanio.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class InfligirDanio : MonoBehaviour
 {
-    [Tooltip("1 = medio corazón, 2 = un corazón")]
+    [Tooltip("1 = medio corazón, 2 = un corazón, 3 = corazón y medio...")]
     public int danioMitadCorazones = 1;
 
     private void OnTriggerEnter(Collider other)
