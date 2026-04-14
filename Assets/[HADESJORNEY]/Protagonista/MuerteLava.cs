6dd0d6a886e874d0c084d3e65c322b7d86d3abe7
mar :) <mcuegil@upv.edu.es>
2026-04-14 09:36:50 +0200

    mejora cs

--- Assets/[HADESJORNEY]/Protagonista/MuerteLava.cs
+++ Assets/[HADESJORNEY]/Protagonista/MuerteLava.cs
@@ -4,7 +4,6 @@ public class MuerteLava : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // Buscamos el script MuerteJugador en Alastor (o en sus padres si choca la cápsula)
         MuerteJugador scriptMuerte = other.GetComponentInParent<MuerteJugador>();
 
         if (scriptMuerte != null)
