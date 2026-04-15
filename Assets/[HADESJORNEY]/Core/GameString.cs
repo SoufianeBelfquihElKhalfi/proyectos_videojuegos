e7df6a91f7cdaf41a65a52d476e06d2009a1253c
Soufiane <soufianebelfquih828@gmail.com>
2026-04-15 18:46:15 +0200

    borrado todo a la verga

--- Assets/[HADESJORNEY]/Core/GameString.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameString", menuName = "Core/GameString")]
-public class GameString : ScriptableObject
-{
-    [SerializeField] private string m_value;
-
-    public string Value { get => m_value; }
-}
--- Assets/[HADESJORNEY]/Enemigos/MuerteEnemigo.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class MuerteEnemigo : MonoBehaviour
-{
-    public void Morir()
-    {
-        Destroy(gameObject);
-    }
-}
