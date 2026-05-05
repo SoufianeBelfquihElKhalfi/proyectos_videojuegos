38a6cc3c553de7034a2a398baf40aa42705f15d7
Soufiane <soufianebelfquih828@gmail.com>
2026-05-05 12:20:07 +0200

    vidasereinicia

--- Assets/[HADESJORNEY]/Core/DatosGlobales.cs
+++ Assets/[HADESJORNEY]/Core/DatosGlobales.cs
@@ -3,4 +3,11 @@ public static class DatosGlobales
     public static int vidaActual;
     public static int vidaMaxima;
     public static bool hayDatosVida = false;
+
+    public static void ReiniciarPartida()
+    {
+        vidaActual = 0;
+        vidaMaxima = 0;
+        hayDatosVida = false;
+    }
 }
