1bb9b99706910572ba9e06fb1ea32c5316d0e8df
Aitor Segura Arribas <gatotico2015@gmail.com>
2026-06-10 17:58:02 +0200

    Pasos enemigos otra vez
    Bombardeen murcia

--- Assets/[HADESJORNEY]/Enemigos/SonidosEnemigos.cs
+++ Assets/[HADESJORNEY]/Enemigos/SonidosEnemigos.cs
@@ -8,6 +8,16 @@ public class SonidosEnemigos : MonoBehaviour
     [SerializeField] private string idAtaque = "enemigo_ataque";
     [SerializeField] private string idRecibirGolpe = "recibir_golpe_enemigo";
     [SerializeField] private string idMuerte = "muerte_enemigo";
+    [SerializeField] private string idPisada = "pisada_enemigo";
+
+    [Header("Variación pisadas")]
+    [SerializeField] private float pitchMinPisada = 0.92f;
+    [SerializeField] private float pitchMaxPisada = 1.08f;
+    [SerializeField] private float volumenMinPisada = 0.75f;
+    [SerializeField] private float volumenMaxPisada = 1f;
+    [SerializeField] private float tiempoMinimoEntrePisadas = 0.12f;
+
+    private float tiempoUltimaPisada = -999f;
 
     public void ReproducirAlerta()
     {
@@ -29,6 +39,22 @@ public class SonidosEnemigos : MonoBehaviour
         Reproducir3D(idMuerte);
     }
 
+    public void ReproducirPisada()
+    {
+        if (Time.time - tiempoUltimaPisada < tiempoMinimoEntrePisadas)
+            return;
+
+        tiempoUltimaPisada = Time.time;
+
+        Reproducir3DConVariacion(
+            idPisada,
+            pitchMinPisada,
+            pitchMaxPisada,
+            volumenMinPisada,
+            volumenMaxPisada
+        );
+    }
+
     private void Reproducir3D(string idSonido)
     {
         if (string.IsNullOrWhiteSpace(idSonido))
@@ -40,5 +66,31 @@ public class SonidosEnemigos : MonoBehaviour
         }
 
         AudioManager.Instance.ReproducirSFX3D(idSonido, transform.position);
+    }
+
+    private void Reproducir3DConVariacion(
+        string idSonido,
+        float pitchMin,
+        float pitchMax,
+        float volumenMin,
+        float volumenMax
+    )
+    {
+        if (string.IsNullOrWhiteSpace(idSonido))
+            return;
+
+        if (AudioManager.Instance == null)
+        {
+            throw new MissingReferenceException($"{name}: falta AudioManager en la escena.");
+        }
+
+        AudioManager.Instance.ReproducirSFX3DConVariacion(
+            idSonido,
+            transform.position,
+            pitchMin,
+            pitchMax,
+            volumenMin,
+            volumenMax
+        );
     }
 }
